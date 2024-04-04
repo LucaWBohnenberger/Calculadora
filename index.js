9967382659be4e435c189981cd931c387210712e
Luca <lucabohneberger@gmail.com>
2024-04-04 15:59:50 -0300

    problema do parenteses reolvido, inicio de códico para calular dois valores(possivelmente inutil).

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 function debug(){
-    alert("Ta ai")
+    console.log(calculoVerdadeiro)
+    console.log(calculoVerdadeiro2)
 }
 
 calculo1 = ""
 calculo2 = ""
-calculoVerdadeiro = null
+calculoVerdadeiro = ""
+calculoVerdadeiro2 = ""
+controle_calculo = true
+var vezes, menos, div, percent, parentese, mais, habilitador
+habilitador = true
+
 
 function atualizar(){
     var mudar =  document.getElementById("numeros_grandes")
     mudar.innerHTML = calculo1
+    var mudar2 = document.getElementById("numeros_pequenos")
+    mudar2.innerHTML = calculo2
 }
+
 
 function addnumero(num){
     calculo1 += String(num)
+    if (controle_calculo){
+        calculoVerdadeiro += String(num)
+    }else{
+        calculoVerdadeiro2 += String(num)
+    }
     console.log(calculo1)
-    calculoVerdadeiro += calculo1
+    controle_por_fora = true
     atualizar()
 }
 
@@ -22,37 +36,93 @@ function addnumero(num){
 function apagar_tudo(){
     calculo1 = ""
     calculo2 = ""
+    calculoVerdadeiro = ""
     atualizar()
 }
 
 function addvezes(){
+    if(habilitador){
     calculo1 += "x"
-
+    calculo2 += calculo1
+    calculo1 = ""
+    mais = true
+    }else{
+        calcular()
+    }
 }
 
 function adddiv(){
+    if(habilitador){
     calculo1 += "/"
+    calculo2 += calculo1
+    calculo1 = ""
+    div = true
+    }else{
+    calcular()
+    }
 }
 
 function addpercent(){
+    if(habilitador){
     calculo1 += "%"
+    calculo2 += calculo1
+    calculo1 = ""
+    percent =true
+    }else{
+        calcular()
+    }
 }
 
 function addmais(){
+    if(habilitador){
     calculo1 += "+"
+    calculo2 += calculo1
+    calculo1 = ""
+    mais = true
+    }else{
+        calcular()
+    }
 }
 
 function addmenos(){
+    if(habilitador){
     calculo1 += "-"
+    calculo2 += calculo1
+    calculo1 = ""
+    menos = false
+    } else{
+        calcular()
+    }
 }
 
 function addponto(){
-    calculo1 += "."
+    if (!calculo1.includes(".")){
+        calculo1 += "."
+    }
 }
 
+var controle = true
+var controle_por_fora = true
+
 function addpa(){
-    if (!calculo1.includes("(")){
-        calculo1 += "\u0029"
-        atualizar()
+    if (controle){
+        calculo1 += "("
+        controle = false
+        controle_por_fora = false
+
+    }else if (controle_por_fora) {
+        calculo1 += ")"
+        controle = true
     }
+    atualizar()
+}
+
+
+
+function calcular(){
+    calculo2 += calculo1
+    controle_calculo = false
+
+    atualizar()
+
 }
