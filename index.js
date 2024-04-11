11abc87a9c1a88ed36efa97b239c9922d8a87a82
Luca <lucabohneberger@gmail.com>
2024-04-11 15:44:28 -0300

    Calculadora funcional, leve modificação no funcionamento

--- index.js
+++ index.js
@@ -2,28 +2,24 @@ calculo1 = ""
 calculo2 = ""
 operacao = ""
 segurar_operacao = ""
-igual = true
+var mudar =  document.getElementById("numeros_pequenos")
+var mudar1 =  document.getElementById("numeros_grandes")
 
 function atualizar(){
-    var mudar =  document.getElementById("numeros_pequenos")
     mudar.innerHTML = calculo1
     mudar.innerHTML += operacao
-    var mudar1 =  document.getElementById("numeros_grandes")
     mudar1.innerHTML = ""
 }
 
 
 function addnumero(num){
-    
-    var mudar =  document.getElementById("numeros_grandes")
-    if (operacao.length == 1){
+    if (operacao.length == 1 & calculo2.length < 11){
         segurar_operacao = ""
         calculo2 += String(num)
-        mudar.innerHTML = calculo2
-    }else if (igual){
+        mudar1.innerHTML = calculo2
+    }else if (calculo1.length < 11){
         calculo1 += String(num)
-        mudar.innerHTML = calculo1
-        console.log(calculo1)
+        mudar1.innerHTML = calculo1
     }
 }
 
@@ -42,29 +38,27 @@ function addvezes(){
     atualizar()
     }else if (segurar_operacao == "" & operacao.length < 3){
         segurar_operacao = "x"
-        calcular(false)
+        calcular()
     }
 }
 
 function adddiv(){
     if(operacao.length < 1 & calculo1.length > 0){
-    habilitador = false
     operacao = "/"
     atualizar()
     }else if (segurar_operacao == "" & operacao.length < 3){
         segurar_operacao = "/"
-        calcular(false)
+        calcular()
     }
 }
 
 function addpercent(){
     if(operacao.length < 1 & calculo1.length > 0){
-    habilitador = false
     operacao = "%"
     atualizar()
     }else if (segurar_operacao == "" & operacao.length < 3){
         segurar_operacao = "%"
-        calcular(false)
+        calcular()
     }
 }
 
@@ -74,7 +68,7 @@ function addmais(){
     atualizar()
     }else if (segurar_operacao == "" & operacao.length < 3){
         segurar_operacao = "+"
-        calcular(false)
+        calcular()
     }
 }
 
@@ -83,7 +77,7 @@ function addmenos(){
     operacao = "-"
     }else if (segurar_operacao == "" & operacao.length < 3){
         segurar_operacao = "-"
-        calcular(false)
+        calcular()
     }
     atualizar()
 }
@@ -95,18 +89,15 @@ function addsinal(){
         }else{
             calculo1 = "-" + calculo1
         }
-        var mudar =  document.getElementById("numeros_grandes")
-        mudar.innerHTML = calculo1
+        mudar1.innerHTML = calculo1
     
     }else if(calculo1.length > 0){
-        console.log(operacao.length)
         if (calculo2.includes("-")){
             calculo2 = calculo2.replace("-", "")
         }else{
             calculo2 = "-" + calculo2
         }
-        var mudar =  document.getElementById("numeros_grandes")
-        mudar.innerHTML = calculo2
+        mudar1.innerHTML = calculo2
     }
 }
 
@@ -125,64 +116,58 @@ function addponto(){
 function remove(){
     if (operacao.length > 0){
         calculo2 = calculo2.slice(0, -1)
-        var mudar =  document.getElementById("numeros_grandes")
-        mudar.innerHTML = calculo2
+        mudar1.innerHTML = calculo2
     }else{
         calculo1 = calculo1.slice(0, -1)
-        var mudar =  document.getElementById("numeros_grandes")
-        mudar.innerHTML = calculo1
+        mudar1.innerHTML = calculo1
     }
 }
 
 
-function calcular(condicional){
-    if (calculo1 != "Resultado Indefinido"){
+function calcular(){
+    if (calculo2.endsWith("0") || calculo2.endsWith("1") || calculo2.endsWith("2") || calculo2.endsWith("3")
+    || calculo2.endsWith("3") || calculo2.endsWith("4") || calculo2.endsWith("5") || calculo2.endsWith("6") 
+    || calculo2.endsWith("7") || calculo2.endsWith("8") || calculo2.endsWith("9") ){
+
         calculo1 = Number(calculo1)
         calculo2 = Number(calculo2)
     
-    switch(operacao){
-        case "+":
-            calculo1 = calculo1 + calculo2
-            break;
+        switch(operacao){
+            case "+":
+                calculo1 = calculo1 + calculo2
+                break;
 
-        case "-":
-            calculo1 = calculo1 - calculo2
-            break;
+            case "-":
+                calculo1 = calculo1 - calculo2
+                break;
 
-        case "x":
-            calculo1 = calculo1 * calculo2
-            break;
+            case "x":
+                calculo1 = calculo1 * calculo2
+                break;
 
-        case "/":
-            if (calculo2 == 0){
-                calculo1 = "Resultado Indefinido"
-            }else{
-                calculo1 = calculo1 / calculo2
-            }
-            break;
+            case "/":
+                if (calculo2 == 0){
+                    calculo1 = "Resultado Indefinido"
+                }else{
+                    calculo1 = calculo1 / calculo2
+                }
+                break;
 
-        case "%":
-            calculo1 = calculo1 % calculo2
-            calculo1 = calculo1.toFixed(3)
-            break;
-    }
-    calculo2 = ""
-    if (calculo1 == "Resultado Indefinido" || isNaN(calculo1) || calculo1 == "NaN" || calculo1 == "NaN-"){
-        calculo1 == "Resultado Indefinido"
-        operacao = "    "
-        console.log("opa")
-    }else{
-        operacao = segurar_operacao
-    }
+            case "%":
+                if (calculo2 == 0){
+                    calculo1 = "Resultado Indefinido"
+                }else{
+                    calculo1 = calculo1 % calculo2
+                }
+                break;
+        }
+        calculo2 = ""
+        if (calculo1 == "Resultado Indefinido"){
+            operacao = "    "
+        }else{
+            operacao = segurar_operacao
+        }
 
-
-
-    if (!condicional){
         atualizar()
-    }else{
-        igual = false
-        atualizar()
-    }
-
     }
 }
